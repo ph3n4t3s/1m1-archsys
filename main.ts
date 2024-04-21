1d25de065483eb2b3fd1e87fa94aa217cd48ce4a
ph3n4t3s <105867089+ph3n4t3s@users.noreply.github.com>
2024-04-21 03:04:51 +0200

    Mise à jour main.blocks, main.ts, exemple.md

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 basic.forever(function () {
-    serial.writeValue("temperature", input.temperature())
-    serial.writeValue("luminosite", input.lightLevel())
-    basic.pause(1000)
+    serial.writeValue("D Out", pins.digitalReadPin(DigitalPin.P0))
+    serial.writeValue("A Out", pins.analogReadPin(AnalogPin.P0))
+    basic.pause(100)
 })
