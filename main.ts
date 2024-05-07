bf2da73690fdfbf6fe2dd483e1dd03b723a06dff
ph3n4t3s <105867089+ph3n4t3s@users.noreply.github.com>
2024-05-07 19:13:51 +0200

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,20 @@
+function LectureCapteurs () {
+    basic.showIcon(IconNames.SmallDiamond)
+    mesure_D_Out = pins.digitalReadPin(DigitalPin.P0)
+    mesure_A_Out = pins.analogReadPin(AnalogPin.P1)
+}
+function EnvoieDonnees () {
+    basic.showIcon(IconNames.SmallSquare)
+    serial.writeValue("D Out", mesure_D_Out)
+    radio.sendValue("D Out", mesure_D_Out)
+    serial.writeValue("A Out", mesure_A_Out)
+    radio.sendValue("A Out", mesure_A_Out)
+}
+let mesure_A_Out = 0
+let mesure_D_Out = 0
+radio.setGroup(10)
 basic.forever(function () {
-    serial.writeValue("D Out", pins.digitalReadPin(DigitalPin.P0))
-    serial.writeValue("A Out", pins.analogReadPin(AnalogPin.P1))
-    basic.pause(1000)
+    basic.showIcon(IconNames.Square)
+    LectureCapteurs()
+    EnvoieDonnees()
 })
